820455541edadd7bc784ab08bfae7ce595dae6c4
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Korean translations for candy-query.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'database.no_file' => 'candy-query: SQLite 파일을 찾을 수 없음: {path}',
+    'cli.usage'        => '사용법: candy-query <sqlite파일>',
+];
