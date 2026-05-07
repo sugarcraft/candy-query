59e58463a8c125261c58c50ed4ae44765624896c
Joe Huss <detain@interserver.net>
2026-05-07 05:50:43 -0400

    Add Japanese locale for all libs (#254)

---
+++ lang/ja.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Japanese translations for candy-query.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'database.no_file' => 'candy-query：SQLite ファイルが見つかりません：{path}',
+    'cli.usage'        => '用法：candy-query <sqliteファイル>',
+];
